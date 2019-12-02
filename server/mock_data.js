19a17e0cbb746b99c0dc320f6548d8762ad1bcd7
Lilly Tang <lillytang@Lillys-MBP.attlocal.net>
2019-12-02 00:50:20 -0500

    seed 100 dishes with random reviews

--- server/mock_data.js
+++ server/mock_data.js
@@ -1,42 +1,41 @@
 const { PopularDishes, PopularDishesReview } = require("./model.js");
 var faker = require("faker");
 
-var review = {
-  reviewer_name: faker.name.findName(),
-  reviewer_photo: faker.image.avatar(),
-  review_date: faker.date.recent(),
-  review_text: faker.lorem.paragraph()
-};
+for (var i = 0; i < 100; i++) {
+  var dish_id = Math.floor(Math.random() * 100000);
 
-var dish = {
-  dish_id: Math.floor(Math.random() * 120),
-  dish_name: faker.lorem.words(),
-  reviewer_photo: faker.image.abstract()
-};
+  var dish = {
+    dish_id: dish_id,
+    dish_name: faker.lorem.words(),
+    reviewer_photo: faker.image.abstract()
+  };
 
-console.log("this script is running");
-
-PopularDishes.create(dish, (err, data) => {
-  if (err) {
-    res.send(err);
-  } else {
-    res.send(data);
-    
-    review.dish_id = dish.dish_id;
-
-    PopularDishesReview.create(review, (err, data) => {
-      if (err) {
-        res.send(err);
-      } else {
-      }
-    });
-  }
-});
-
-PopularDishesReview.create(review, (err, data) => {
-  if (err) {
-    res.send(err);
-  } else {
-    res.send(data);
-  }
-});
+  PopularDishes.create(dish, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+  PopularDishes.get((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      data.forEach(dish => {
+        var review = {
+          dish_id: dish.dish_id,
+          reviewer_name: faker.name.findName(),
+          reviewer_photo: faker.image.avatar(),
+          review_date: faker.date.recent(),
+          review_text: faker.lorem.paragraph()
+        };
+        PopularDishesReview.create(review, (err, data) => {
+          if (err) {
+            res.send(err);
+          } else {
+          }
+        });
+      });
+    }
+  });
+}
--- server/model.js
+++ server/model.js
@@ -31,7 +31,8 @@ module.exports = {
   },
   PopularDishesReview: {
     create: (fakerObj, callback) => {
-      var query = `INSERT INTO reviews (reviewer_name, review_date, review_text) values ('${fakerObj.reviewer_name}', '${fakerObj.review_date}', '${fakerObj.review_text}');`;
+
+      var query = `INSERT INTO reviews (dish_id, reviewer_name, review_date, review_text) values (${fakerObj.dish_id}, '${fakerObj.reviewer_name}', '${fakerObj.review_date}', '${fakerObj.review_text}');`;
       connection.query(query, function(error, result) {
         if (error) {
           console.log(error);
